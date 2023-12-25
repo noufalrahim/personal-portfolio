99a31fc1c7675b9e2b00bb807ad87443b8a616f2
noufalrahim <noufalrahim6784@gmail.com>
2023-12-25 07:46:57 +0530

    Created a static website

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -12,7 +12,7 @@ export function Footer() {
                         color="blue-gray"
                         className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 text-white"
                     >
-                        &copy; 2023 Noufal Rahim                    </Typography>
+                    © 2023 Noufal Rahim                    </Typography>
                 </li>
             </ul>
         </footer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,11 @@ import HomePage from '../components/Home';
 import { NavbarMenu } from '@/components/Navbar';
 import SocialConnect from '@/components/SocialConnect';
 import AboutMe from '@/components/AboutMe';
-import Head from 'next/head';
 import { Footer } from '@/components/Footer';
 import ContactMe from '@/components/ContactMe';
 export default function Home() {
   return (
     <>
-      <Head>
-        <script src="https://kit.fontawesome.com/0a8e40b305.js" crossOrigin="anonymous"></script>
-      </Head>
       <NavbarMenu />
       <main className="relative z-20 flex min-h-screen flex-col items-center justify-between py-24 px-20 overflow-y-auto mt-16 lg:mt-0 md:mt-0 xl:mt-0">
         <HomePage />
