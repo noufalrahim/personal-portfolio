82fc5b7b0e3bb2a29549921875b5887b3c2ce286
noufalrahim <noufalrahim6784@gmail.com>
2023-12-25 07:51:15 +0530

    Created a static website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import HomePage from '../components/Home';
 import { NavbarMenu } from '@/components/Navbar';
-// import SocialConnect from '@/components/SocialConnect';
-// import AboutMe from '@/components/AboutMe';
-// import { Footer } from '@/components/Footer';
-// import ContactMe from '@/components/ContactMe';
+import SocialConnect from '@/components/SocialConnect';
+import AboutMe from '@/components/AboutMe';
+import { Footer } from '@/components/Footer';
+import ContactMe from '@/components/ContactMe';
 export default function Home() {
   return (
     <>
       <NavbarMenu />
       <main className="relative z-20 flex min-h-screen flex-col items-center justify-between py-24 px-20 overflow-y-auto mt-16 lg:mt-0 md:mt-0 xl:mt-0">
-        <HomePage />
-        {/* <SocialConnect />
+        {/* <HomePage /> */}
+        <SocialConnect />
         <AboutMe />
         <ContactMe />
-        <Footer /> */}
+        <Footer />
       </main>
       <div className='fixed inset-0 z-0 bg-black'>
         <img src='/bg.jpg' alt='' className='opacity-30 w-full h-full object-cover' />
