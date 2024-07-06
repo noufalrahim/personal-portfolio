639d8f71bf487b0407d98cb9e0d99a14f16efecd
noufalrahim <noufalrahim6784@gmail.com>
2024-07-06 09:40:58 +0530

    Removed script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script src="https://smtpjs.com/v3/smtp.js"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
